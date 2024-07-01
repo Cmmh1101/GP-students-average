2d71351eada56c6193e231c2abceae3ad991cb66
Carla Montano <cm.emprendiendo@gmail.com>
2024-07-01 00:21:34 -0500

    finished requirements

--- GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6821f73847b7822cdbdb29022bec07e0426dac30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8df044d817ee5a43af608d819c0dbf390c7b9ed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
